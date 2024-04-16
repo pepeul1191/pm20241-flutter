0c118fd186bfc2e4e8c50c6cd463eacc17fe43b6
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2024-04-15 22:36:37 -0500

    commit inicial

--- lib/main.dart
+++ lib/main.dart
@@ -7,119 +7,153 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      home: Scaffold(
+          body: Stack(children: [
+        Container(
+          color: Colors.blue,
+          margin: EdgeInsets.all(
+              00.0), // Margen inferior de 20.0 (puedes ajustar este valor)
+          alignment: Alignment.centerLeft,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        Container(
+          color: Colors.green,
+          height: 350,
+          margin: EdgeInsets.all(
+              00.0), // Margen inferior de 20.0 (puedes ajustar este valor)
+          alignment: Alignment.centerLeft,
+        ),
+        Container(
+          color: Colors.transparent,
+          margin: EdgeInsets.only(
+              top: 200,
+              left: 100,
+              right:
+                  100), // Margen inferior de 20.0 (puedes ajustar este valor)
+          alignment: Alignment.topCenter,
+          child: Column(
+            children: [
+              Container(
+                  margin: EdgeInsets.only(
+                      bottom: 10), // Margen de 8 en todos los lados
+                  child: Text(
+                    'Logo',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  )),
+              Text(
+                'Gimnasio ULima',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              )
+            ],
+          ),
+        ),
+        Container(
+            color: Colors.white,
+            alignment: Alignment.center,
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.fromLTRB(
+              MediaQuery.of(context).size.width * 0.1, // Margen izquierdo
+              200, // Margen superior
+              MediaQuery.of(context).size.width * 0.1, // Margen derecho
+              MediaQuery.of(context).size.width * 0.1, // Margen inferior
+            ),
+            height: 400,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.network(
+                  'https://upload.wikimedia.org/wikipedia/commons/0/0e/ULIMA_logo.png', // URL de la imagen
+                  width: 40, // Ancho de la imagen
+                  height: 40, // Alto de la imagen
+                  fit: BoxFit.cover, // Ajuste de la imagen
+                ),
+                Text('Ingresa esta información',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    )),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText:
+                            'Ingrese su contraseña', // Etiqueta del campo de texto
+                        border:
+                            OutlineInputBorder(), // Borde del campo de texto
+                      ),
+                    ),
+                    Text('Contraseña',
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        )),
+                    SizedBox(
+                      width: double.infinity, // Ocupar todo el ancho disponible
+                      child: TextButton(
+                        onPressed: () {
+                          // Función que se ejecuta cuando se presiona el botón
+                          print('Botón presionado');
+                        },
+                        style: TextButton.styleFrom(
+                          backgroundColor:
+                              Colors.blue, // Color de fondo del botón
+                          padding: EdgeInsets
+                              .zero, // Padding cero para eliminar el espacio interno
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius
+                                .zero, // Bordes cero para eliminar los bordes
+                          ),
+                        ),
+                        child: Text(
+                          'Botón sin Bordes',
+                          style: TextStyle(
+                            color: Colors.white, // Color del texto
+                          ),
+                        ),
+                      ),
+                    ),
+                    /*ElevatedButton(
+                      onPressed: () {
+                        // Función que se ejecuta cuando se presiona el botón
+                        print('Botón presionado');
+                      },
+                      child: Text('Login'),
+                    ), */
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('No tienes una cuenta?',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        )),
+                    Text('Créala aquí',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.cyanAccent,
+                        )),
+                  ],
+                )
+              ],
+            )),
+      ])),
     );
   }
 }
